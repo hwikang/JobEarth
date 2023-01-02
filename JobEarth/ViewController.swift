ebadd6634e3bdfefc654ce7d5c52360b96bba96b
hwikang <khdrogba@gmail.com>
2023-01-02 16:15:16 +0900

    Project Init

---
+++ JobEarth/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JobEarth
+//
+//  Created by Dumveloper on 2023/01/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
